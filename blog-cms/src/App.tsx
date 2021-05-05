907a9ac827553bc254c11e9bc034256003c5a70c
Huxley Dowling <hdowling87@hotmail.com>
2021-05-05 19:36:09 +1000

    Placeholder dashboard page and routing. Remove login component to fix naming convention.

--- blog-cms/src/App.tsx
+++ blog-cms/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Login from './Components/Login/login';
+import Login from './Components/Login/Login';
+import Dashboard from './Components/Dashboard/Dashboard';
 
 export default function App() {
   return (
@@ -10,6 +11,10 @@ export default function App() {
           <Route path="/">
             <Login />
           </Route>
+          <Route path="/Dashboard">
+            <Dashboard />
+          </Route>
+          ;
         </Switch>
       </div>
     </Router>
---
+++ blog-cms/src/Components/Dashboard/Dashboard.tsx
@@ -0,0 +1,5 @@
+import React, { useReducer, useEffect } from 'react';
+
+const Dashboard = () => <div>Logged in!</div>;
+
+export default Dashboard;
