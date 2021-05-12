5dee5399c7ae43bea8801eaa84917d87069de01d
Huxley Dowling <hdowling87@hotmail.com>
2021-05-12 21:26:50 +1000

    Remove app store

--- blog-cms/src/Components/Hooks/useAppStore.ts
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line import/no-named-as-default
-import appStore, { AppStore } from './useAppStore/appStore';
-
-export function useAppStore(): AppStore {
-  return appStore;
-}
--- blog-cms/src/Components/Hooks/useAppStore/appStore.ts
+++
@@ -1,8 +0,0 @@
-import Cookies from 'universal-cookie';
-
-const cookies = new Cookies();
-
-export class AppStore {}
-
-export const appStore = new AppStore();
-export default appStore;
