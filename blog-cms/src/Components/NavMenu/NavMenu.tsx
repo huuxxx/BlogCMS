97d970c6a3ac0146c3fec261de4d440814a8a071
Huxley Dowling <hdowling87@hotmail.com>
2021-07-22 17:32:16 +1000

    Import RGS. Hide Nav sm xs

--- blog-cms/src/Components/NavMenu/NavMenu.tsx
+++ blog-cms/src/Components/NavMenu/NavMenu.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link as Route, Redirect } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import Cookies from 'universal-cookie';
+import { Hidden } from 'react-grid-system';
 import { useGlobalContext } from '../../Store';
 import './NavMenu.css';
 
@@ -18,33 +19,35 @@ const NavMenu = () => {
   };
 
   return (
-    <div className="sidenav">
-      <div className="sidenav-contents">
-        <span className="userName">User - {userName}</span>
-        <nav className="sidenav-links">
-          <ul>
-            <li>
-              <Route to="/app/dashboard">Home</Route>
-            </li>
-            <li>
-              <Route to="/app/blogs">Blogs</Route>
-            </li>
-            <li>
-              <Route to="/app/newblog">New Blog</Route>
-            </li>
-          </ul>
-        </nav>
+    <Hidden sm xs>
+      <div className="sidenav">
+        <div className="sidenav-contents">
+          <span className="userName">User - {userName}</span>
+          <nav className="sidenav-links">
+            <ul>
+              <li>
+                <Route to="/app/dashboard">Home</Route>
+              </li>
+              <li>
+                <Route to="/app/blogs">Blogs</Route>
+              </li>
+              <li>
+                <Route to="/app/newblog">New Blog</Route>
+              </li>
+            </ul>
+          </nav>
 
-        <Button
-          variant="contained"
-          size="small"
-          color="inherit"
-          onClick={logOut}
-        >
-          Log Out
-        </Button>
+          <Button
+            variant="contained"
+            size="small"
+            color="inherit"
+            onClick={logOut}
+          >
+            Log Out
+          </Button>
+        </div>
       </div>
-    </div>
+    </Hidden>
   );
 };
 
