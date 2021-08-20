29cfef178b2ead41ff00c705372ada172110cd9c
Huxley Dowling <hdowling87@hotmail.com>
2021-08-21 01:02:37 +1000

    ImageTiles individual state

--- blog-cms/src/Components/Images/ImageTile.tsx
+++ blog-cms/src/Components/Images/ImageTile.tsx
@@ -1,52 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, CircularProgress } from '@material-ui/core';
+import { AxiosResponse } from 'axios';
+import Cookies from 'universal-cookie';
+import ConfirmModal from '../Modals/ConfirmModal';
 import './ImageTile.css';
+
+const DELETE_BLOG_ENDPOINT = process.env.REACT_APP_ENDPOINT_IMAGE_DELETE;
+
+const cookies = new Cookies();
+const axios = require('axios').default;
 
 interface IProps {
   item: string;
-  showModal: (show: boolean) => void;
-  deleteId: (show: string) => void;
-  loading: boolean;
-  imageDeleted: boolean;
-  buttonState: boolean;
 }
 
-const ImageTile: React.FC<IProps> = ({
-  item,
-  showModal,
-  deleteId,
-  loading,
-  imageDeleted,
-  buttonState,
-}) => (
-  <>
-    <img
-      src={
-        // eslint-disable-next-line prefer-template
-        'https://blogapi.huxdev.com/Images/' + item
-      }
-      alt={item}
-      width="200px"
-      height="200px"
-      style={{ border: 'solid' }}
-      className={imageDeleted ? 'image-deleted' : ''}
-    />
-    <Button
-      variant="contained"
-      size="large"
-      color="secondary"
-      style={{ marginTop: '55px', marginLeft: '-150px' }}
-      onClick={() => {
-        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-        showModal(true);
-        deleteId(item);
-      }}
-      disabled={buttonState}
-    >
-      Delete
-    </Button>
-    <div className="loadingSpinner">{loading ? <CircularProgress /> : ''}</div>
-  </>
-);
+const ImageTile: React.FC<IProps> = ({ item }) => {
+  const [loading, setLoading] = useState(false);
+  const [imageDeleted, setImageDeleted] = useState(false);
+  const [buttonState, setButtonState] = useState(false);
+  const [showModal, setshowModal] = useState(false);
+
+  const handleDeleteImage = () => {
+    setLoading(true);
+    setButtonState(true);
+    axios
+      .post(
+        DELETE_BLOG_ENDPOINT,
+        {
+          id: item,
+        },
+        {
+          headers: { Authorization: `Bearer ${cookies.get('token')}` },
+        }
+      )
+      .then((response: AxiosResponse) => {
+        if (response.status === 200) {
+          setLoading(false);
+          setImageDeleted(true);
+        }
+      })
+      .catch((error: string) => {
+        setLoading(false);
+        setButtonState(false);
+      });
+  };
+
+  return (
+    <>
+      <ConfirmModal
+        confirmButton={handleDeleteImage}
+        show={showModal}
+        setShow={setshowModal}
+        message="Delete Image?"
+      />
+      <img
+        src={
+          // eslint-disable-next-line prefer-template
+          'https://blogapi.huxdev.com/Images/' + item
+        }
+        alt={item}
+        width="200px"
+        height="200px"
+        style={{ border: 'solid' }}
+        className={imageDeleted ? 'image-deleted' : ''}
+      />
+      <Button
+        variant="contained"
+        size="large"
+        color="secondary"
+        style={{ marginTop: '55px', marginLeft: '-150px' }}
+        onClick={() => setshowModal(true)}
+        disabled={buttonState}
+      >
+        Delete
+      </Button>
+      <div className="loadingSpinner">
+        {loading ? <CircularProgress /> : ''}
+      </div>
+    </>
+  );
+};
 
 export default ImageTile;
--- blog-cms/src/Components/Images/ImagesPage.tsx
+++ blog-cms/src/Components/Images/ImagesPage.tsx
@@ -1,24 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { AxiosResponse } from 'axios';
-import Cookies from 'universal-cookie';
-import ConfirmModal from '../Modals/ConfirmModal';
 import ImageTile from './ImageTile';
 import NavMenu from '../NavMenu/NavMenu';
 import './ImagesPage.css';
 
-const cookies = new Cookies();
 const axios = require('axios').default;
 
 const GET_IMAGES_ENDPOINT = process.env.REACT_APP_ENDPOINT_IMAGES_GET_ALL;
-const DELETE_BLOG_ENDPOINT = process.env.REACT_APP_ENDPOINT_IMAGE_DELETE;
 
 const ImagesPage = () => {
   const [responseData, setResponseData] = useState<[string]>();
-  const [showModal, setshowModal] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [imageDelte, setImageDeleted] = useState(false);
-  const [buttonState, setButtonState] = useState(false);
-  const [deleteId, setDeleteId] = useState('');
 
   useEffect(() => {
     axios
@@ -32,53 +23,15 @@ const ImagesPage = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const handleDeleteImage = () => {
-    setLoading(true);
-    setButtonState(true);
-    axios
-      .post(
-        DELETE_BLOG_ENDPOINT,
-        {
-          id: deleteId,
-        },
-        {
-          headers: { Authorization: `Bearer ${cookies.get('token')}` },
-        }
-      )
-      .then((response: AxiosResponse) => {
-        if (response.status === 200) {
-          setLoading(false);
-          setImageDeleted(true);
-        }
-      })
-      .catch((error: string) => {
-        setLoading(false);
-        setButtonState(false);
-      });
-  };
-
   return (
     <div className="page-parent">
       <div className="page-sub-parent">
         <NavMenu />
-        <ConfirmModal
-          confirmButton={handleDeleteImage}
-          show={showModal}
-          setShow={setshowModal}
-          message="Delete Image?"
-        />
         <h1>Images</h1>
         <div className="images-page-item-container">
           {responseData?.map((item) => (
             <div key={item}>
-              <ImageTile
-                item={item}
-                showModal={setshowModal}
-                deleteId={setDeleteId}
-                loading={loading}
-                imageDeleted={imageDelte}
-                buttonState={buttonState}
-              />
+              <ImageTile item={item} />
             </div>
           ))}
         </div>
