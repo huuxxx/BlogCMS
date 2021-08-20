7036d000e91edc65db51d07b0f13a49f1b39d1dc
Huxley Dowling <hdowling87@hotmail.com>
2021-08-20 20:19:42 +1000

    Image mngr deletion

--- blog-cms/src/Components/Images/ImagesPage.tsx
+++ blog-cms/src/Components/Images/ImagesPage.tsx
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { AxiosResponse } from 'axios';
-import { Button } from '@material-ui/core';
+import { Button, CircularProgress } from '@material-ui/core';
+import Cookies from 'universal-cookie';
+import ConfirmModal from '../Modals/ConfirmModal';
 import NavMenu from '../NavMenu/NavMenu';
 import './ImagesPage.css';
 
+const cookies = new Cookies();
 const axios = require('axios').default;
 
 const GET_IMAGES_ENDPOINT = process.env.REACT_APP_ENDPOINT_IMAGES_GET_ALL;
-
-type ImageResponseItem = {
-  file: string;
-};
+const DELETE_BLOG_ENDPOINT = process.env.REACT_APP_ENDPOINT_IMAGE_DELETE;
 
 const ImagesPage = () => {
   const [responseData, setResponseData] = useState<[string]>();
+  const [showModal, setshowModal] = useState(false);
+  const [buttonState, setButtonState] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [deleteId, setDeleteId] = useState('');
+  const [imageDeleted, setImageDeleted] = useState(false);
 
   useEffect(() => {
     axios
@@ -27,32 +32,75 @@ const ImagesPage = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const handleDeleteImage = () => {
+    setLoading(true);
+    setButtonState(true);
+    axios
+      .post(
+        DELETE_BLOG_ENDPOINT,
+        {
+          id: deleteId,
+        },
+        {
+          headers: { Authorization: `Bearer ${cookies.get('token')}` },
+        }
+      )
+      .then((response: AxiosResponse) => {
+        if (response.status === 200) {
+          setLoading(false);
+          setImageDeleted(true);
+        }
+      })
+      .catch((error: string) => {
+        setLoading(false);
+        setButtonState(false);
+      });
+  };
+
   return (
     <div className="page-parent">
       <div className="page-sub-parent">
         <NavMenu />
+        <ConfirmModal
+          confirmButton={() => {
+            handleDeleteImage();
+          }}
+          show={showModal}
+          setShow={setshowModal}
+          message="Delete Image?"
+        />
         <h1>Images</h1>
         <div className="images-page-item-container">
           {responseData?.map((item) => (
             <div key={item}>
               <img
-                // eslint-disable-next-line prefer-template
-                src={'https://blogapi.huxdev.com/Images/' + item.toString()}
+                src={
+                  // eslint-disable-next-line prefer-template
+                  'https://blogapi.huxdev.com/Images/' + item.toString()
+                }
                 alt={item.toString()}
                 width="200px"
                 height="200px"
                 style={{ border: 'solid' }}
+                className={imageDeleted ? 'image-deleted' : ''}
               />
               <Button
                 variant="contained"
                 size="large"
                 color="secondary"
                 style={{ marginTop: '55px', marginLeft: '-150px' }}
-                //   onClick={deleteButton}
-                //   disabled={buttonState}
+                onClick={() => {
+                  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+                  setshowModal(true);
+                  setDeleteId(item.toString());
+                }}
+                disabled={buttonState}
               >
                 Delete
               </Button>
+              <div className="loadingSpinner">
+                {loading ? <CircularProgress /> : ''}
+              </div>
             </div>
           ))}
         </div>
