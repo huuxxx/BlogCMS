20ef2eca7a2b718360907738163b0e8c2de13e60
Huxley Dowling <hdowling87@hotmail.com>
2024-07-16 19:35:32 +1000

    Remove login reducer

--- blog-cms/src/Components/Login/Login.tsx
+++ blog-cms/src/Components/Login/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useState } from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -16,89 +16,36 @@ const axios = require('axios').default;
 
 const LOGIN_ENDPOINT = process.env.REACT_APP_ENDPOINT_LOGIN;
 
-type State = {
-  username: string;
-  password: string;
-  isButtonDisabled: boolean;
-  helperText: string;
-  isError: boolean;
-};
-
-const initialState: State = {
-  username: '',
-  password: '',
-  isButtonDisabled: true,
-  helperText: '',
-  isError: false,
-};
-
-type Action =
-  | { type: 'setUsername'; payload: string }
-  | { type: 'setPassword'; payload: string }
-  | { type: 'setIsButtonDisabled'; payload: boolean }
-  | { type: 'loginSuccess'; payload: string }
-  | { type: 'loginFailed'; payload: string }
-  | { type: 'setIsError'; payload: boolean };
-
-const reducer = (state: State, action: Action): State => {
-  switch (action.type) {
-    case 'setUsername':
-      return {
-        ...state,
-        username: action.payload,
-      };
-    case 'setPassword':
-      return {
-        ...state,
-        password: action.payload,
-      };
-    case 'setIsButtonDisabled':
-      return {
-        ...state,
-        isButtonDisabled: action.payload,
-      };
-    case 'loginSuccess':
-      return {
-        ...state,
-        helperText: action.payload,
-        isError: false,
-      };
-    case 'loginFailed':
-      return {
-        ...state,
-        helperText: action.payload,
-        isError: true,
-      };
-    case 'setIsError':
-      return {
-        ...state,
-        isError: action.payload,
-      };
-    default:
-      return {
-        ...state,
-      };
-  }
-};
+interface LoginRequest {
+  UserName: string;
+  Password: string;
+}
 
 const Login = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [password, setPassword] = useState('');
+  const [userName, setUserName] = useState('');
   const history = useHistory();
   const [loading, setLoading] = useState(false);
   const { setIsLoggedIn } = useGlobalContext();
-  const { setUserName } = useGlobalContext();
+  const { setUserName: setLoggedInUserName } = useGlobalContext();
+  const [helperText, setHelperText] = useState('');
+  const [isError, setIsError] = useState(false);
+
+  const loginRequest: LoginRequest = {
+    UserName: userName,
+    Password: password,
+  };
 
   const handleLogin = () => {
     setLoading(true);
     axios
       .post(LOGIN_ENDPOINT, {
-        username: state.username,
-        password: state.password,
+        loginRequest,
       })
       .then((response) => {
         if (response.status === 200) {
           setIsLoggedIn(true);
-          setUserName(state.username);
+          setLoggedInUserName(userName);
           const obj = JSON.parse(JSON.stringify(response.data));
           cookies.set('token', obj.token);
           history.push('/app/dashboard');
@@ -106,10 +53,8 @@ const Login = () => {
       })
       .catch(() => {
         setLoading(false);
-        dispatch({
-          type: 'loginFailed',
-          payload: 'Login error',
-        });
+        setHelperText('Login error');
+        setIsError(true);
       });
   };
 
@@ -122,20 +67,15 @@ const Login = () => {
   const handleUsernameChange: React.ChangeEventHandler<HTMLInputElement> = (
     event
   ) => {
-    dispatch({
-      type: 'setUsername',
-      payload: event.target.value,
-    });
+    setUserName(event.target.value);
   };
 
   const handlePasswordChange: React.ChangeEventHandler<HTMLInputElement> = (
     event
   ) => {
-    dispatch({
-      type: 'setPassword',
-      payload: event.target.value,
-    });
+    setPassword(event.target.value);
   };
+
   return (
     <div>
       <form className="container" noValidate autoComplete="off">
@@ -145,7 +85,7 @@ const Login = () => {
             <div>
               <TextField
                 spellCheck={false}
-                error={state.isError}
+                error={isError}
                 fullWidth
                 id="username"
                 type="email"
@@ -157,14 +97,14 @@ const Login = () => {
               />
               <TextField
                 spellCheck={false}
-                error={state.isError}
+                error={isError}
                 fullWidth
                 id="password"
                 type="password"
                 label="Password"
                 placeholder="Password"
                 margin="normal"
-                helperText={state.helperText}
+                helperText={helperText}
                 onChange={handlePasswordChange}
                 onKeyPress={handleKeyPress}
               />
