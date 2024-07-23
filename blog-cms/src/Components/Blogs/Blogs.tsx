3340d46217b5c46a06f45e65841af080a873d448
Huxley Dowling <hdowling87@hotmail.com>
2024-07-23 19:10:07 +1000

    Improved blog view styling

--- blog-cms/src/Components/Blogs/Blogs.tsx
+++ blog-cms/src/Components/Blogs/Blogs.tsx
@@ -29,13 +29,13 @@ const Blogs = () => {
     setPage(newPage);
   };
 
-const getBlogs = () => {
+  const getBlogs = () => {
     axios
       .get(BLOGS_ENDPOINT, {
         params: {
-            page: page,
-            pageSize: pageSize,
-          },
+          page: page,
+          pageSize: pageSize,
+        },
       })
       .then((response: AxiosResponse) => {
         if (response.status === 200) {
@@ -45,27 +45,29 @@ const getBlogs = () => {
         }
       })
       .catch((error: string) => {});
-}
+  };
 
   useEffect(() => {
     getBlogs();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page]);
 
   return (
     <div className="page-parent">
       <div className="page-sub-parent blogs-parent">
         <NavMenu />
-        <h1>Blogs</h1>
-        <Button
-          variant="contained"
-          size="small"
-          color="inherit"
-          onClick={() => history.push('/newblog')}
-          style={{ marginTop: '25px' }}
-        >
-          + Create Blog
-        </Button>
+        <div>
+          <h1>Blogs</h1>
+          <Button
+            variant="contained"
+            size="small"
+            color="inherit"
+            onClick={() => history.push('/newblog')}
+            //   style={{ marginTop: '10px' }}
+          >
+            + Create Blog
+          </Button>
+        </div>
         {responseData?.map((item) => (
           <div
             key={item.id.toString()}
@@ -75,19 +77,25 @@ const getBlogs = () => {
             <Link to={`editblog/${item.id}`}>
               <h3>{item.title}</h3>
             </Link>
-            <span>{formatDateDetailed(item.dateCreated)}</span>
+            <span style={{ whiteSpace: 'nowrap', overflow: 'scroll' }}>
+              {formatDateDetailed(item.dateCreated)}
+            </span>
           </div>
         ))}
-        {!loading &&
+        <div
+          style={{ marginBottom: '1em', position: 'absolute', width: '50%' }}
+        >
+          {!loading && (
             <TablePagination
-                component="div"
-                count={count}
-                page={page}
-                onPageChange={handleChangePage}
-                rowsPerPage={pageSize}
-                rowsPerPageOptions={[ 10 ]}
+              component="div"
+              count={count}
+              page={page}
+              onPageChange={handleChangePage}
+              rowsPerPage={pageSize}
+              rowsPerPageOptions={[10]}
             />
-        }
+          )}
+        </div>
       </div>
     </div>
   );
